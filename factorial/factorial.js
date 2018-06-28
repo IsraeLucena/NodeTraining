99f76ccd316a80d1952cd5e65efbb0fc949bd0de
IsraeLucena <israelclucena@gmail.com>
2018-06-28 18:41:43 -0300

    novos treinos

---
+++ factorial/factorial.js
@@ -0,0 +1,13 @@
+const factorial = (num) => {
+    if (num === 0) {
+        return 1
+    }
+    return num * factorial(num - 1)
+}
+
+//exports.factorial = factorial
+
+// module.exports = { factorial: factorial }
+
+module.exports = factorial
+
--- globalAndProcess.js
+++ globalAndProcess.js
@@ -1,3 +1,5 @@
+const factorial = require('./factorial')
+
 console.log('Function n-factorial')
 console.log(`Executing the script on directory ${process.cwd()}`)
 
@@ -5,11 +7,5 @@ process.on('exit', () => {
     console.log('The scrip is about to end')
 })
 
-const factorial = (num) => {
-    if (num === 0) {
-        return 1
-    }
-    return num * factorial(num - 1)
-}
 const num = parseInt(process.argv[2])
 console.log(`The factorial of ${num} is equals to ${factorial(num)}`)
